ff448df3c3d8d49643db8073a1a75f5e2e502517
parvinderBabar <er.kaurparvinder@gmail.com>
2023-11-29 19:10:17 -0500

    toggle

--- exercises/21-transforming-dom/transforming-dom.js
+++ exercises/21-transforming-dom/transforming-dom.js
@@ -5,4 +5,26 @@
  */
 (function () {
   // Put your answers in here
+  const image = document.querySelector('#imageCat');
+  // console.log(ref);
+  image.src= " https://media.giphy.com/media/3oKIPnAiaMCws8nOsE/giphy.gif";
+  console.log(image);
+  //change link
+  const chngeLink = document.querySelector('a[href="https://www.w3schools.com/js/default.asp"]');
+  // console.log(chngeLink);
+  chngeLink.href = "https://developer.mozilla.org/en-US/docs/Web/JavaScript";
+  console.log(chngeLink);
+/*
+<div class="alert alert-success">
+        Change the text inside this <em>&lt;div&gt;</em> to say "I am
+        victorious!"
+      </div>
+*/
+  const textChange = document.querySelector('#id1');
+  textChange.innerHTML = 'I am victorious!';
+  console.log(textChange);
 })();
+
+//chnage background color
+const bgChange = document.querySelector('.alert alert-info');
+bgChange.sty
--- exercises/23-toggle-events/toggle-events.js
+++ exercises/23-toggle-events/toggle-events.js
@@ -1,4 +1,17 @@
-(function () {
+// (function () {
+  // <body>
+  //   <section id="acceptCookiesPopup" class="bg-dark text-white sticky">
+  //     <div class="container pt-4 pb-4">
+  //       <p>
+  //         We use cookies to give you the best online experience, measure your
+  //         visits to our sites and to enable marketing activities (including with
+  //         our marketing partners).
+  //       </p>
+  //       <button id="acceptCookiesButton" class="btn btn-success">
+  //         Accept Cookies
+  //       </button>
+  //     </div>
+  //   </section>
   /**
    * Problem 1: Dismiss the "Accept Cookies" popup
    *
@@ -23,4 +36,33 @@
    * but you can switch up the HTML so that it uses inline styles if you like.
    */
   // Write your answer here
-})();
+// })();
+const popupBtn = document.querySelector('#acceptCookiesButton');
+  const popupSection = document.querySelector('#acceptCookiesPopup');
+  popupBtn.addEventListener('click', () => {
+    popupSection.style.display = 'none';
+  })
+
+
+
+// const cookieDismiss = document.querySelector('acceptCookiesButton');
+
+//  const popupSection = document.querySelector('#acceptCookiesPopup');
+// cookieDismiss.addEventListener('click', () => {
+//   popupSection.style.display = 'none';
+// });
+const toggleComments = document.querySelector('#toggleComments');
+toggleComments.addEventListener('click', () => {
+
+   if (comments.classList.contains("hidden")) {
+    
+     comments.classList.remove("hidden");
+     toggleComments.innerHTML = 'Hide Comments';
+  } else {
+    
+     comments.classList.add("hidden");
+      toggleComments.innerHTML = 'View Comments';
+  }
+  } 
+)
+ 
