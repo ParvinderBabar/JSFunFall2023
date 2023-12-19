eabce6e141e2924f8d4866e8d05253ce5ca52364
parvinderBabar <er.kaurparvinder@gmail.com>
2023-12-18 19:12:35 -0500

    commit

--- exercises/31-ajax-series/ajax-series.js
+++ exercises/31-ajax-series/ajax-series.js
@@ -1,4 +1,4 @@
-(function () {
+// (function () {
   /**
    *
    * As a user, I should be able to a pick Rick and Morty character from a drop-down, and it should display an image of that character.
@@ -15,6 +15,79 @@
    * with the character's ID:
    * https://rickandmortyapi.com/api/character/2
    *
-   * You must make two AJAX request to solve this problem.
+   * You must make two AJAX requests to solve this problem.
    */
+  // const responseFromApi = // What you get back from the Rick and Morty characters API
+  // let htmlStr = "<option></option>";
+  // for (let character of responseFromApi.results) {
+  //   htmlStr += `<option value="${character.id}">${character.name}</option>`;
+//   document.addEventListener("DOMContentLoaded", async function () {
+//     try {
+//       const dropdown = document.querySelector("#dropdown");
+//       const characterImage = document.querySelector("#characterImage");
+
+//       // Fetch the list of characters
+//       const request = await fetch("https://rickandmortyapi.com/api/character");
+//       const data = await request.json();
+
+//       // Assuming the character data has a property like 'results' containing an array of characters
+//       data.results.forEach((character) => {
+//         const option = document.createElement("option");
+//         option.value = character.id; // Set the value based on your character data
+//         option.text = character.name; // Set the text based on your character data
+//         dropdown.add(option);
+//       });
+
+//       // Add event listener for dropdown change
+//       dropdown.addEventListener("change", async function () {
+//         const selectedCharacterId = this.value;
+
+//         // Fetch individual character data
+//         const request = await fetch(`https://rickandmortyapi.com/api/character/${selectedCharacterId}`);
+//         const data = await request.json();
+
+//         // Update the image source
+//         characterImage.src = data.image;
+//       });
+
+//       console.log(data);
+//     } catch (err) {
+//       console.error("Error", err);
+//     }
+//   });
+// })();
+(async function () {
+  
+  const response = await axios({
+    method: "GET",
+    url: "https://rickandmortyapi.com/api/character",
+  });
+
+  const dropdown = document.querySelector("#dropdown");
+
+  response.data.results.forEach((character) => {
+    // console.log(character.id);
+    // <option value="_____">______</option>
+    const option = document.createElement("option");
+    option.text = character.name;
+    option.value = character.id;
+    dropdown.appendChild(option);
+  });
+
+  dropdown.addEventListener("change", async (e) => {
+    // const 
+    console.log(e.target.value);
+    const Image = document.querySelector("#Image");
+    const heading = document.querySelector('#title-head');
+   
+const request = await fetch(`https://rickandmortyapi.com/api/character/${e.target.value}`);
+    
+    console.log(request);
+    const data = await request.json();
+
+    Image.src = data.image;
+    heading.textContent = data.name;
+    console.log(Image.src);
+
+  });
 })();
